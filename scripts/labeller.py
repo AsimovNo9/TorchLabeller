31e2113337a94eedadc98b975e125a4316dfdc68
Adedamola Sode <adedamola.sode@gmail.com>
2022-06-27 11:23:45 +0200

    Creating a sort of boiler plate

--- scripts/labeller.py
+++ scripts/labeller.py
@@ -1,2 +1,17 @@
 import torch
-import 
+import os 
+
+class Model():
+    def __init__(self, model_path, model_name):
+        self.model_path = model_path
+        self.model_name = model_name
+
+    def loadModel(self):
+        self.model = torch.hub.load('self.model_path', self.model_name, pretrained=True)
+
+    def modelDetect(self, img_path):
+        self.results = self.model(img_path)
+        return self.results
+
+    def makeLabels(self, img_path):
+        
