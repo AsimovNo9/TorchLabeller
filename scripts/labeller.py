034b43b3eaa13538549430ae5c18c005e4854004
Adedamola Sode <adedamola.sode@gmail.com>
2022-07-18 15:05:09 +0200

    Worked on it after a while

--- scripts/labeller.py
+++ scripts/labeller.py
@@ -1,17 +1,38 @@
 import torch
-import os 
+import os
+import glob
+import cv2
 
-class Model():
-    def __init__(self, model_path, model_name):
+
+class Model:
+    def __init__(self, model_origin, model_type, model_path):
+        self.model_origin = model_origin
+        self.model_type = model_type
         self.model_path = model_path
-        self.model_name = model_name
+        self.makeLabels("./images")
 
     def loadModel(self):
-        self.model = torch.hub.load('self.model_path', self.model_name, pretrained=True)
+        self.model = torch.hub.load(
+            self.model_origin, self.model_type, path=self.model_path
+        )
 
-    def modelDetect(self, img_path):
-        self.results = self.model(img_path)
-        return self.results
+    def modelDetect(self, image):
+        self.results = self.model(image)
+        self.detections = self.results.pandas().xyxy[0].values.tolist()
+        return self.detections
 
     def makeLabels(self, img_path):
-        
+        self.loadModel()
+        images = []
+        for img in glob.glob(f"{img_path}/*.png"):
+            print(img)
+            read_image = cv2.imread(img)
+            images.append(read_image)
+
+        for image in images:
+            detections = self.makeLabels(image)
+            print(detections)
+
+
+if __name__ == "__main__":
+    Model("ultralytics/yolov5", "custom", "../utils/yolov5n6.pt")
